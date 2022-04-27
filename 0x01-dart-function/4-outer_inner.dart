d1d5e2417ad4c09fa6054f7bdeebe9bff1b8b756
breezybrow <browder.bree@gmail.com>
2022-04-27 16:47:30 -0500

    update outer inner

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,11 +1,9 @@
 void outer(String name, String id) {
+  String inner() {
+    var splitter = name.split(" ");
+    var alias = splitter[1].substring(0, 1) + "." + splitter[0];
+    return "Hello Agent ${alias} your id is $id";
+  }
 
-  String welcome = "Hello Agent B.$name your id is $id";
-
-  print("${welcome}");
-}
-
-String inner() {
-
-  return("\n");
+  print(inner());
 }
