7272acc5aef85261d3c09b6a437731e21c18acf4
breezybrow <browder.bree@gmail.com>
2022-04-28 15:03:54 -0500

    add Quotes dir

--- 0x03-asynchronous/breaking_bad.dart
+++ 0x03-asynchronous/breaking_bad.dart
@@ -8,7 +8,7 @@ printBbCharacters() async {
     var json = jsonDecode(res.body);
 
     for (var idx = 0; idx < json.length; idx++) {
-      print("${json[idx]["name"]}");
+      print("${json[idx]['name']}");
     }
   } catch (err) {
     print('error caught: $err');
