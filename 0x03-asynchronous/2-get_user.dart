0628ee910a5f44d81d52c6f98942c900acbc9ecc
breezybrow <browder.bree@gmail.com>
2022-04-28 14:08:24 -0500

    not recognizing fetchUser()

---
+++ 0x03-asynchronous/2-get_user.dart
@@ -0,0 +1,12 @@
+Future<void> getUser() async {
+try {
+  print(await fetchUser());
+} catch (err) {
+  print('error caught: $err');
+}
+
+Future<String> fetchUser() => Future.delayed(
+      const Duration(seconds: 2),
+      () => throw 'Cannot locate user',
+    );
+}
