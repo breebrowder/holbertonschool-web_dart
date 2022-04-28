39016c387146b7a798494e1f42d115977ec6c4eb
breezybrow <browder.bree@gmail.com>
2022-04-28 14:14:16 -0500

    it was missing import

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -7,9 +7,3 @@ Future<void> getUser() async {
     print('error caught: $err');
   }
 }
-
-
-Future<String> fetchUser() => Future.delayed(
-      const Duration(seconds: 2),
-      () => throw 'Cannot locate user',
-    );
