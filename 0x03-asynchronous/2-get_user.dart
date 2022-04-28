c194568f90422a0892ba00e0cea18ff63292b248
breezybrow <browder.bree@gmail.com>
2022-04-28 14:27:29 -0500

    update 2-get_user

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -1,13 +1,12 @@
-Future<void> getUser() async {
-  try {
-    final String data = await fetchUser();
-    print(data);
-  } catch (err) {
-    print('error caught: $err');
-  }
-}
-
 Future<String> fetchUser() => Future.delayed(
       const Duration(seconds: 2),
       () => throw 'Cannot locate user',
     );
+    
+getUser() async {
+  try {
+    print(await fetchUser());
+  } catch (err) {
+    print('error caught: $err');
+  }
+}
