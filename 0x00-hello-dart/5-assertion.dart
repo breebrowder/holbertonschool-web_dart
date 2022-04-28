1519c79bf9b77de38bb84091ffb4112957a08bce
breezybrow <browder.bree@gmail.com>
2022-04-27 19:41:30 -0500

    fix output_error doesn't contain nb

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -1,8 +1,8 @@
 void main(List<String> args) {
-  int num = int.parse(args[0]);
+  int nb = int.parse(args[0]);
   String str = "You Passed";
   
-  assert(num >= 80, 'Uncaught Error: Assertion failed: "The score must be bigger or equal to 80');
+  assert(nb >= 80, 'Uncaught Error: Assertion failed: "The score must be bigger or equal to 80');
  
-  print(str);
+    print(str);
 }
