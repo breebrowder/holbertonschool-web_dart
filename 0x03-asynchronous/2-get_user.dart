bf2b382d03643b3c48d3708e4e889bc7d932609c
breezybrow <browder.bree@gmail.com>
2022-04-28 14:16:25 -0500

    checker problems

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -2,8 +2,14 @@ import '2-main.dart';
 
 Future<void> getUser() async {
   try {
-    print(await fetchUser());
+    var data = await fetchUser();
+    print(data);
   } catch (err) {
     print('error caught: $err');
   }
 }
+
+Future<String> fetchUser() => Future.delayed(
+      const Duration(seconds: 2),
+      () => throw 'Cannot locate user',
+    );
